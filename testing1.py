180ab61e792c9dc6925c7a11acf99b19975e2caa
Yaakov <yaakovoriya1@gmail.com>
2024-04-30 20:00:43 +0300

    Update code

--- testing1.py
+++ testing1.py
@@ -1,7 +1,16 @@
 
 print("Hello world")
+print("Please enter")
  # ? What we have learned?
  # * we learned that for create a new repository of git we need to write 
- # * git init
- # *  then we can track for changes in our file using git add
+ # * git init on this repository.
+ # *  then we can track for changes in our file using git add file_name
+#* after adding the changes , we can do git status to see whats going on 
+# ! dont forget to put git add file_name to update the status for the last change before commiting 
+ # * saving tha chnages - git commit 
+# * if we want to see the commits(our history of changes) we can write git log
 
+
+# * after one commit , if you want to edit it and commit it again 
+# * make the changes , run the code, and the rest is known
+# * you can do git diff to see the chnages from the last code
