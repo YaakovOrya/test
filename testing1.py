f13091a39e05185bc5c202834fdbd81514ea5afc
Yaakov Haiby <yaakovhaiby@h-MacBook-Pro-sl-Yaakov.local>
2024-04-30 19:37:41 +0300

    first commit

---
+++ testing1.py
@@ -0,0 +1,7 @@
+
+print("Hello world")
+ # ? What we have learned?
+ # * we learned that for create a new repository of git we need to write 
+ # * git init
+ # *  then we can track for changes in our file using git add
+
